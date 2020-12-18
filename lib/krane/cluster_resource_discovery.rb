8efba14b495f504f6ac3b8ccd74c540516406856
Daniel Turner <daniel.turner@shopify.com>
2020-12-18 15:55:44 -0800

    Update cluster discovery

--- lib/krane/cluster_resource_discovery.rb
+++ lib/krane/cluster_resource_discovery.rb
@@ -17,88 +17,57 @@ module Krane
     end
 
     def prunable_resources(namespaced:)
-      black_list = %w(Namespace Node ControllerRevision)
-      api_versions = fetch_api_versions
-
-      fetch_resources(namespaced: namespaced).uniq { |r| r['kind'] }.map do |resource|
-        next unless resource['verbs'].one? { |v| v == "delete" }
-        next if black_list.include?(resource['kind'])
-        group_versions = api_versions[resource['apigroup'].to_s]
-        version = version_for_kind(group_versions, resource['kind'])
-        [resource['apigroup'], version, resource['kind']].compact.join("/")
+      black_list = %w(Namespace Node ControllerRevision NodeProxyOptions)
+      fetch_resources(namespaced: namespaced).uniq { |r| r["kind"] }.map do |resource|
+        next unless resource["verbs"].one? { |v| v == "delete" }
+        next if black_list.include?(resource["kind"])
+        version = resource["version"]
+        [resource["apigroup"], version, resource["kind"]].compact.join("/")
       end.compact
     end
 
-    # kubectl api-resources -o wide returns 5 columns
-    # NAME SHORTNAMES APIGROUP NAMESPACED KIND VERBS
-    # SHORTNAMES and APIGROUP may be blank
-    # VERBS is an array
-    # serviceaccounts sa <blank> true ServiceAccount [create delete deletecollection get list patch update watch]
     def fetch_resources(namespaced: false)
-      command = %w(api-resources)
-      command << "--namespaced=#{namespaced}"
-      raw, err, st = kubectl.run(*command, output: "wide", attempts: 5,
-        use_namespace: false)
-      if st.success?
-        rows = raw.split("\n")
-        header = rows[0]
-        resources = rows[1..-1]
-        full_width_field_names = header.downcase.scan(/[a-z]+[\W]*/)
-        cursor = 0
-        fields = full_width_field_names.each_with_object({}) do |name, hash|
-          start = cursor
-          cursor = start + name.length
-          # Last field should consume the remainder of the line
-          cursor = 0 if full_width_field_names.last == name.strip
-          hash[name.strip] = [start, cursor - 1]
-        end
-        resources.map do |resource|
-          resource = fields.map { |k, (s, e)| [k.strip, resource[s..e].strip] }.to_h
-          # Manually parse verbs: "[get list]" into %w(get list)
-          resource["verbs"] = resource["verbs"][1..-2].split
-          resource
-        end
-      else
-        raise FatalKubeAPIError, "Error retrieving api-resources: #{err}"
+      api_paths.flat_map do |path|
+        resources = fetch_api_path(path)["resources"] || []
+        resources.map { |r| resource_hash(path, namespaced, r) }.compact
       end
     end
 
     private
 
-    # kubectl api-versions returns a list of group/version strings e.g. autoscaling/v2beta2
-    # A kind may not exist in all versions of the group.
-    def fetch_api_versions
-      raw, err, st = kubectl.run("api-versions", attempts: 5, use_namespace: false)
-      # The "core" group is represented by an empty string
-      versions = { "" => %w(v1) }
-      if st.success?
-        rows = raw.split("\n")
-        rows.each do |group_version|
-          group, version = group_version.split("/")
-          versions[group] ||= []
-          versions[group] << version
-        end
+    def api_paths
+      raw_json, err, st = kubectl.run("get", "--raw", "/", attempts: 5, use_namespace: false)
+      paths = if st.success?
+        JSON.parse(raw_json)["paths"]
       else
-        raise FatalKubeAPIError, "Error retrieving api-versions: #{err}"
+        raise FatalKubeAPIError, "Error retrieving raw path /: #{err}"
       end
-      versions
+      paths.select { |path| path.start_with?("/api") }
     end
 
-    def version_for_kind(versions, kind)
-      # Override list for kinds that don't appear in the lastest version of a group
-      version_override = { "CronJob" => "v1beta1", "VolumeAttachment" => "v1beta1",
-                           "CSIDriver" => "v1beta1", "Ingress" => "v1beta1",
-                           "CSINode" => "v1beta1", "Job" => "v1",
-                           "IngressClass" => "v1beta1", "FrontendConfig" => "v1beta1",
-                           "ServiceNetworkEndpointGroup" => "v1beta1" }
+    def fetch_api_path(path)
+      raw_json, err, st = kubectl.run("get", "--raw", path, attempts: 5, use_namespace: false)
+      if st.success?
+        JSON.parse(raw_json)
+      else
+        raise FatalKubeAPIError, "Error retrieving api path: #{err}"
+      end
+    end
 
-      pattern = /v(?<major>\d+)(?<pre>alpha|beta)?(?<minor>\d+)?/
-      latest = versions.sort_by do |version|
-        match = version.match(pattern)
-        pre = { "alpha" => 0, "beta" => 1, nil => 2 }.fetch(match[:pre])
-        [match[:major].to_i, pre, match[:minor].to_i]
-      end.last
-      version_override.fetch(kind, latest)
+    def resource_hash(path, namespaced, blob)
+      return unless blob["namespaced"] == namespaced
+      return unless blob["verbs"] && blob["kind"]
+
+      path_regex = /(\/apis?\/)(?<group>[^\/]*)\/?(?<version>v.+)/
+      match = path.match(path_regex)
+      group = match[:group]
+      version = match[:version]
+      {
+        "verbs" => blob["verbs"],
+        "kind" => blob["kind"],
+        "apigroup" => group,
+        "version" => version,
+      }
     end
 
     def fetch_crds
